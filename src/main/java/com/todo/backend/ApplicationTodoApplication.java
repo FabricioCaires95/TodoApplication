8d1dda9d236a0d3c6d503637b53c73c6985e580c
Fabricio <fabricio.vieira995@hotmail.com>
2021-09-01 19:17:30 -0300

    configs iniciais

---
+++ src/main/java/com/todo/backend/ApplicationTodoApplication.java
@@ -0,0 +1,13 @@
+package com.todo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTodoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationTodoApplication.class, args);
+  }
+
+}
