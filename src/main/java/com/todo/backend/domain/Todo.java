8d5d248ce7e5edfb50cea57d94ff58c03d54addf
Fabricio <fabricio.vieira995@hotmail.com>
2021-09-01 21:40:57 -0300

    transformando domain em entity

--- src/main/java/com/todo/backend/domain/Todo.java
+++ src/main/java/com/todo/backend/domain/Todo.java
@@ -1,15 +1,36 @@
 package com.todo.backend.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Data;
 
 @Data
-public class Todo {
+@Entity
+public class Todo implements Serializable {
 
-    private Long id;
-    private String title;
-    private String description;
-    private LocalDateTime deadline;
-    private Boolean status;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String description;
+  private LocalDateTime deadline;
+  private Boolean status;
 
+  public Todo() {
+    super();
+  }
+
+  public Todo(Long id, String title, String description, LocalDateTime deadline,
+      Boolean status) {
+    super();
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.deadline = deadline;
+    this.status = status;
+  }
 }
