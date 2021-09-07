67920e87cf9d3243dba1e21b631349eba617f5fe
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-09-07 19:06:28 -0300

    dto especifico para update

---
+++ src/main/java/com/todo/backend/dto/TodoUpdateDto.java
@@ -0,0 +1,35 @@
+package com.todo.backend.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoUpdateDto {
+
+    @NotNull(message = "ID is required")
+    private Long id;
+
+    @NotEmpty(message = "name is required")
+    private String title;
+
+    @NotEmpty(message = "description is required")
+    private String description;
+
+    @FutureOrPresent
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime deadline;
+
+    private Boolean status;
+}
