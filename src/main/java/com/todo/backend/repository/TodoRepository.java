9a855d05b687bfcc0d1bb5f2b3d993b8e827cf0a
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-09-17 01:38:38 -0300

    paginacao no metodo findAll no repository

--- src/main/java/com/todo/backend/repository/TodoRepository.java
+++ src/main/java/com/todo/backend/repository/TodoRepository.java
@@ -1,17 +1,13 @@
 package com.todo.backend.repository;
 
 import com.todo.backend.domain.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    List<Todo> findAllByStatusFalseOrderByDeadline();
-
-    List<Todo> findAllByStatusTrueOrderByDeadline();
-
-
+    Page<Todo> findAllByIsFinished(Pageable pageable, boolean isFinished);
 }
