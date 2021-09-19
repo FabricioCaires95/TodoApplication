5e0fd36e31155e8e1815547c119183150b07e590
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-09-19 16:40:27 -0300

    test para os metodos mapper

---
+++ src/test/java/com/todo/backend/mapper/TodoMapperTest.java
@@ -0,0 +1,57 @@
+package com.todo.backend.mapper;
+
+
+import com.todo.backend.domain.Todo;
+import com.todo.backend.dto.TodoDto;
+import com.todo.backend.dto.TodoUpdateDto;
+import org.mapstruct.factory.Mappers;
+import org.testng.annotations.Test;
+
+import static com.todo.backend.utils.TodoUtils.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class TodoMapperTest {
+
+
+    private TodoMapper todoMapper = Mappers.getMapper(TodoMapper.class);
+
+    @Test
+    public void shoudConvertToDto() {
+        Todo entity = getTodoEntity();
+        TodoDto dto = todoMapper.convertToDto(entity);
+
+        assertNotNull(dto);
+        assertEquals(dto.getId(), entity.getId());
+        assertEquals(dto.getTitle(), entity.getTitle());
+        assertEquals(dto.getDescription(), entity.getDescription());
+        assertEquals(dto.getDeadline(), entity.getDeadline());
+    }
+
+    @Test
+    public void shoudConvertToEntity() {
+        TodoDto dto = getTodoDto();
+        Todo entity = todoMapper.convertToEntity(dto);
+
+        assertNotNull(entity);
+        assertEquals(entity.getId(), dto.getId());
+        assertEquals(entity.getTitle(), dto.getTitle());
+        assertEquals(entity.getDescription(), dto.getDescription());
+        assertEquals(entity.getDeadline(), dto.getDeadline());
+    }
+
+    @Test
+    public void shouldConvertUpdateDtoToEntity() {
+        TodoUpdateDto updateDto = getUpdateTodoDtoWithComplet();
+        Todo entity = todoMapper.convertUpdateDtoToEntity(updateDto);
+
+        assertNotNull(entity);
+        assertEquals(entity.getId(), updateDto.getId());
+        assertEquals(entity.getTitle(), updateDto.getTitle());
+        assertEquals(entity.getDescription(), updateDto.getDescription());
+        assertEquals(entity.getDeadline(), updateDto.getDeadline());
+    }
+
+
+}
