4bf349f2b6d0acb0daf5bbb81b1d3da7ab837a5b
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-10-02 03:25:36 -0300

    configs para acessar banco mysql

---
+++ src/main/java/com/todo/backend/profiles/DevProfileConfig.java
@@ -0,0 +1,31 @@
+package com.todo.backend.profiles;
+
+
+import com.todo.backend.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevProfileConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() {
+
+        if (!"create".equals(strategy)){
+            return false;
+        }
+        dbService.instanceDatabase();
+
+        return true;
+    }
+}
