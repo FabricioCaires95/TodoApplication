d206e63256cbdfcfd1eaed473b1e8f5970084d4d
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-10-04 01:52:53 -0300

    adicionando cache em alguns metodos

--- src/main/java/com/todo/backend/service/TodoServiceImpl.java
+++ src/main/java/com/todo/backend/service/TodoServiceImpl.java
@@ -1,11 +1,15 @@
 package com.todo.backend.service;
 
+import com.todo.backend.domain.Todo;
 import com.todo.backend.dto.TodoDto;
 import com.todo.backend.dto.TodoUpdateDto;
 import com.todo.backend.exception.NotFoundException;
 import com.todo.backend.mapper.TodoMapper;
 import com.todo.backend.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -21,6 +25,7 @@ public class TodoServiceImpl implements TodoService {
   private TodoMapper mapper;
 
   @Override
+  @Cacheable(cacheNames = Todo.CACHE_NAME, key="#id")
   public TodoDto findById(Long id) {
     return todoRepository.findById(id)
             .map(todo -> mapper.convertToDto(todo))
@@ -28,16 +33,19 @@ public class TodoServiceImpl implements TodoService {
   }
 
   @Override
+  @CacheEvict(cacheNames = Todo.CACHE_NAME, allEntries = true)
   public void createTodo(TodoDto todoDto) {
     todoRepository.save(mapper.convertToEntity(todoDto));
   }
 
   @Override
+  @CacheEvict(cacheNames = Todo.CACHE_NAME, key = "#id")
   public void deleteById(Long id) {
     todoRepository.deleteById(id);
   }
 
   @Override
+  @CachePut(cacheNames = {Todo.CACHE_NAME, TodoUpdateDto.CACHE_NAME}, key = "#todoDto.id")
   public TodoUpdateDto update(TodoUpdateDto todoDto) {
     return todoRepository.findById(todoDto.getId()).map(todo -> {
       todoRepository.save(mapper.convertUpdateDtoToEntity(todoDto));
@@ -46,6 +54,7 @@ public class TodoServiceImpl implements TodoService {
   }
 
   @Override
+  @Cacheable(cacheNames = Todo.CACHE_NAME, key = "#root.method.name")
   public Page<TodoDto> findAllDynamicParameters(Integer page, Integer size, boolean status) {
     PageRequest request = PageRequest.of(page, size,  Sort.by("deadline").ascending());
     return todoRepository
