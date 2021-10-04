6d22886341fda053d6abca8885184cfbcdf300fe
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-10-04 01:50:44 -0300

    classe de config redis e dependencias

--- src/main/java/com/todo/backend/ApplicationTodoApplication.java
+++ src/main/java/com/todo/backend/ApplicationTodoApplication.java
@@ -2,8 +2,10 @@ package com.todo.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApplicationTodoApplication {
 
   public static void main(String[] args) {
