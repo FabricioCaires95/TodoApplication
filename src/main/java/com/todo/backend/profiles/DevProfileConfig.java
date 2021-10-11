d23b7d6e81e905785ee52fb3a0ca6eca824a6711
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-10-10 21:21:03 -0300

    usar h2 no profile dev e mysql no profile de test

--- src/main/java/com/todo/backend/profiles/DevProfileConfig.java
+++ src/main/java/com/todo/backend/profiles/DevProfileConfig.java
@@ -3,7 +3,6 @@ package com.todo.backend.profiles;
 
 import com.todo.backend.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,17 +14,9 @@ public class DevProfileConfig {
     @Autowired
     private DBService dbService;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String strategy;
-
     @Bean
-    public boolean instantiateDatabase() {
-
-        if (!"create".equals(strategy)){
-            return false;
-        }
-        dbService.instanceDatabase();
-
+    public boolean instanceDB() {
+        this.dbService.instanceDatabase();
         return true;
     }
 }
--- src/main/java/com/todo/backend/profiles/TestProfileConfig.java
+++ src/main/java/com/todo/backend/profiles/TestProfileConfig.java
@@ -2,6 +2,7 @@ package com.todo.backend.profiles;
 
 import com.todo.backend.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,9 +14,17 @@ public class TestProfileConfig {
   @Autowired
   private DBService dbService;
 
+  @Value("${spring.jpa.hibernate.ddl-auto}")
+  private String strategy;
+
   @Bean
-  public boolean instanceDB() {
-    this.dbService.instanceDatabase();
+  public boolean instantiateDatabase() {
+
+    if (!"create".equals(strategy)){
+      return false;
+    }
+    dbService.instanceDatabase();
+
     return true;
   }
 }
