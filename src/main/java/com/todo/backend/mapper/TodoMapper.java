b782c650371e19276f94f332097edf4d6048bb23
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-11-28 22:27:46 -0300

    ignorar mapping do user por enquanto

--- src/main/java/com/todo/backend/mapper/TodoMapper.java
+++ src/main/java/com/todo/backend/mapper/TodoMapper.java
@@ -4,18 +4,22 @@ import com.todo.backend.domain.Todo;
 import com.todo.backend.dto.TodoDto;
 import com.todo.backend.dto.TodoUpdateDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TodoMapper {
 
+  @Mapping(target = "user.tasks", ignore = true)
   TodoDto convertToDto(Todo todo);
 
+  @Mapping(target = "user.tasks", ignore = true)
   Todo convertToEntity(TodoDto dto);
 
   Todo convertUpdateDtoToEntity(TodoUpdateDto todoUpdateDto);
 
+  @Mapping(target = "user.tasks", ignore = true)
   List<TodoDto> convertList(List<Todo> todos);
 
 }
