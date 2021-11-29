dd54211891437a722c10e347de639ecb34e9d7d4
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-11-28 22:28:05 -0300

    endpoint para criar user

--- src/main/java/com/todo/backend/controller/UserController.java
+++ src/main/java/com/todo/backend/controller/UserController.java
@@ -3,10 +3,13 @@ package com.todo.backend.controller;
 import com.todo.backend.dto.UserDto;
 import com.todo.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +30,12 @@ public class UserController {
     @GetMapping
     public ResponseEntity<UserDto> getUserByEmail(@RequestParam(value = "email") String email) {
         return ResponseEntity.ok(userService.findUserByEmail(email));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createUser(@RequestBody UserDto userDto) {
+        userService.createUser(userDto);
+       return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
