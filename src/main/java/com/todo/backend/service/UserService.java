af342ee65d460d9fc9c6946558e41f4a96d175a4
Fabricio Caires <fabricio.vieira995@hotmail.com>
2021-12-18 22:20:17 -0300

    metodo para retornar user entity

--- src/main/java/com/todo/backend/service/UserService.java
+++ src/main/java/com/todo/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.todo.backend.service;
 
+import com.todo.backend.domain.User;
 import com.todo.backend.dto.UserDto;
 
 public interface UserService {
@@ -7,6 +8,8 @@ public interface UserService {
     void createUser(UserDto userDto);
 
     UserDto findUserById(Long id);
+
+    User findUserEntity(Long id);
 
     UserDto findUserByEmail(String email);
 }
--- src/main/java/com/todo/backend/service/UserServiceImpl.java
+++ src/main/java/com/todo/backend/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.todo.backend.service;
 
+import com.todo.backend.domain.User;
 import com.todo.backend.dto.UserDto;
 import com.todo.backend.exception.NotFoundException;
 import com.todo.backend.mapper.CycleAvoidingMappingContext;
@@ -39,5 +40,10 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByEmail(email)
                 .map(user -> userMapper.mapToDto(user, avoidingMappingContext))
                 .orElseThrow(() -> new NotFoundException(DEFAULT_NOT_FOUND_MSG));
+    }
+
+    @Override
+    public User findUserEntity(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException(DEFAULT_NOT_FOUND_MSG));
     }
 }
