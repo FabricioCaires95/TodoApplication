a954b7de8ebf49a654d65c0055c944fadb7e1578
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-01-02 18:07:50 -0300

    repositories das roles

---
+++ src/main/java/com/todo/backend/repository/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package com.todo.backend.repository;
+
+import com.todo.backend.domain.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+}
