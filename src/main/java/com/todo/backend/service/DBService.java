66a3795f2c26bad613379a4bda970ac9b846a397
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-01-02 19:24:09 -0300

    incluir roles no start do banco

--- src/main/java/com/todo/backend/service/DBService.java
+++ src/main/java/com/todo/backend/service/DBService.java
@@ -1,7 +1,9 @@
 package com.todo.backend.service;
 
+import com.todo.backend.domain.Role;
 import com.todo.backend.domain.Todo;
 import com.todo.backend.domain.User;
+import com.todo.backend.repository.RoleRepository;
 import com.todo.backend.repository.TodoRepository;
 import com.todo.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,22 @@ public class DBService {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private RoleRepository roleRepository;
+
   public void instanceDatabase() {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    Role r1 = new Role();
+    r1.setName("ROLE_USER");
+
+    Role r2 = new Role();
+    r2.setName("ROLE_ADMIN");
+
     User user = new User(
-            1L, "legend","legend22@gmail.com", "23131eqw", null);
+            1L, "legend","legend22@gmail.com", "{bcrypt}$2a$10$mfT7JIWtFSt2cDKtOT9MC.A1LhPZI8BDbmP74254576LXQIWiMpHW", null , null);
+
+
 
     Todo t1 = new Todo(null,
         "Study",
@@ -37,6 +51,7 @@ public class DBService {
             LocalDate.parse("05/09/2021", formatter),
             false, user);
 
+    roleRepository.saveAll(Arrays.asList(r1,r2));
     userRepository.save(user);
     todoRepository.saveAll(Arrays.asList(t1,t2));
   }
