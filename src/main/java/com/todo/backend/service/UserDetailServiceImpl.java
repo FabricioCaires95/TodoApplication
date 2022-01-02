c8c50a1df1341702e74603dd28443a06b4287e5b
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-01-02 19:24:58 -0300

    ajust implementacao UserDetailServiceImpl

--- src/main/java/com/todo/backend/service/UserDetailServiceImpl.java
+++ src/main/java/com/todo/backend/service/UserDetailServiceImpl.java
@@ -1,14 +1,20 @@
 package com.todo.backend.service;
 
+import com.todo.backend.domain.UserRole;
 import com.todo.backend.repository.UserRepository;
 import com.todo.backend.security.UserSecurityDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Slf4j
@@ -18,13 +24,24 @@ public class UserDetailServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("Authenticating...");
         var user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("UserName Not Found"));
         UserSecurityDto userSecurityDto = new UserSecurityDto();
-        user.getUserRoles().forEach(role -> userSecurityDto.getAuthorites().add(new SimpleGrantedAuthority(role.getRole().getName())));
+        Set<GrantedAuthority> authoritySet = new HashSet<>();
+
+        for (UserRole userRole: user.getUserRoles()){
+            authoritySet.add(new SimpleGrantedAuthority(userRole.getRole().getName()));
+        }
+
+        log.info("Authorites {}", authoritySet);
+
         userSecurityDto.setId(user.getId());
         userSecurityDto.setUsername(user.getEmail());
         userSecurityDto.setPassword(user.getPassword());
+        userSecurityDto.setAuthorites(authoritySet);
+        log.info("User Authenticated {}", userSecurityDto.toString());
         return userSecurityDto;
 
     }
