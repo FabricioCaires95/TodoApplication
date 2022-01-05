c4804b460f729f857e07b8710a3adaaf217d0b6f
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-01-05 05:16:31 -0300

    usar jwt para autenticacao

--- src/main/java/com/todo/backend/security/SecurityConfig.java
+++ src/main/java/com/todo/backend/security/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.todo.backend.security;
 
-import com.todo.backend.service.UserDetailServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,8 +10,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -21,11 +25,18 @@ import java.util.Arrays;
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] PUBLIC_ROUTES = {
+            "/h2-console/**", "/login", "/user/create"
+    };
+
     @Autowired
     private Environment environment;
 
     @Autowired
-    private UserDetailServiceImpl userDetailService;
+    private UserDetailsService userDetailService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,25 +46,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         }
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers(PUBLIC_ROUTES).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtil));
+
 
     }
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder passwordEncoderFactories = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        log.info("Password Encode: {} ", passwordEncoderFactories.encode("23131eqw"));
-        /**auth.inMemoryAuthentication()
-                .withUser("spacer")
-                .password(passwordEncoderFactories.encode("teste1234"))
-                .roles("USER"); **/
+        auth.userDetailsService(userDetailService).passwordEncoder(bCryptPasswordEncoder());
 
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoderFactories);
+    }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "PUT", "DELETE", "GET", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
