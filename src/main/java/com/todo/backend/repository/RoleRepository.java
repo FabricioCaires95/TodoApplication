a2cebe4e747cd2f307aa6f91e5e52858bdcce18e
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-01-05 05:14:54 -0300

    classes apagadas

--- src/main/java/com/todo/backend/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todo.backend.repository;
-
-import com.todo.backend.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
