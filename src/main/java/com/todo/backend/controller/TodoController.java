1bac4d9077f8d7d11aa7bbfa99c3517c04616a4e
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-01-08 01:24:01 -0300

    ajustes de configs

--- src/main/java/com/todo/backend/controller/TodoController.java
+++ src/main/java/com/todo/backend/controller/TodoController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +48,6 @@ public class TodoController {
   }
 
   @GetMapping(value = "/todosByUserId/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  @PreAuthorize("hasAnyRole('ROLE_USER')")
   public ResponseEntity<List<TodoDto>> findAll(@PathVariable Long id) {
     return ResponseEntity.ok(todoService.getTodosByUserId(id));
   }
--- src/main/java/com/todo/backend/security/SecurityConfig.java
+++ src/main/java/com/todo/backend/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +22,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -52,7 +54,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             http.headers().frameOptions().disable();
         }
         http.cors().and().csrf().disable();
-        http.authorizeRequests()
+        http.exceptionHandling().authenticationEntryPoint((request, response, e) -> {
+                    response.sendError(response.SC_UNAUTHORIZED, e.getMessage()
+                    );
+                })
+                .and()
+                .authorizeRequests()
                 .antMatchers(PUBLIC_ROUTES).permitAll()
                 .anyRequest().authenticated()
                 .and()
